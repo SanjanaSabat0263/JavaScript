afcb23dcd8f2e4681eb04fde233f0087f78a678d
Sanjana Sabat <sanjanasabat26@gmail.com>
2025-02-25 20:43:01 +0530

    Add files via upload
    Creating a JS file that explains the concept of comments and datatypes in JS.

---
+++ datatype.js
@@ -0,0 +1,34 @@
+//console.log();
+// single line comments
+/*
+  multiple lines comments
+
+  Datatypes:
+  String
+  Numbers
+  Boolean
+   Bigint
+
+  Null
+    Undefined
+   
+    Symbol
+
+    //Object
+*/
+
+//var score=102;
+
+let score=102;
+let name="Sanjana";
+let isLoggedin = false;
+
+//object
+let teaTypes =["lemon tea","ginger tea","green tea"];
+let user ={firstname:"sanjana",lastname:"sabat"};
+
+console.log(score);
+console.log(name);
+console.log(isLoggedin);
+console.log(teaTypes);
+console.log(user);
