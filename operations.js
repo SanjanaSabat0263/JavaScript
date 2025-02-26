6fc7dfc8137f9e01079235d1bb710409c6ed2ad0
Sanjana Sabat <sanjanasabat26@gmail.com>
2025-02-26 21:38:00 +0530

    Add files via upload
    Performing basic operations

---
+++ operations.js
@@ -0,0 +1,34 @@
+
+let score = 102;
+let bonus=20;
+
+let totalScore=score+bonus;
+
+
+let addition = 4 + 5;
+let subtract= 9 - 3;
+let multiply= 3 * 5;
+let divide= 8 / 2;
+let modulus= 9 % 2;
+let expo= 2 ** 3 ;  //2*2*2
+
+let myscore= 110;
+myscore++;
+// ++myscore
+
+let credits= 56;
+credits--;
+
+//Comparsion operators
+
+let num1= 3;
+let num2=3;
+let num3= 6;
+
+console.log(num1==num2);  //true
+console.log(num1==num3); //false
+console.log(num1!=num3); //true
+console.log(num1<num3);
+console.log(num1>num3);
+console.log(num1<=num3);
+console.log(num1>=num3);
