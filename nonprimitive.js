bec23bb7ba079433ca7bea3a9fc4a7eaa74d9bd0
Sanjana Sabat <sanjanasabat26@gmail.com>
2025-03-06 21:51:21 +0530

    Add files via upload

---
+++ nonprimitive.js
@@ -0,0 +1,29 @@
+const username = {
+    "first name" : "Sanjana", 
+    isLoggedin : true,
+};
+
+username.firstname = "Sanju";
+username.lastname = "sabat";
+
+
+console.log(username["firstname"]);
+console.log(username.lastname);
+console.log(username);
+console.log(typeof username);
+
+let today = new Date();
+console.log(today.getDate());
+
+
+//Array
+
+let heores = ["superman", "batman", "wonderwoman",true];
+console.log(heores);
+console.log(heores[0]);
+console.log(heores.length);
+
+let isValue = "2abc";
+console.log(Number(isValue) + 1);
+
+console.log(Number(null ));
