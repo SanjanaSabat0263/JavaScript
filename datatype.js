f735bff63256df1c6a58cb77f4f568b6db5b73d5
Sanjana Sabat <sanjanasabat26@gmail.com>
2025-03-06 21:50:50 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,37 @@
+console.log("hello");
+// single line comments
+/*
+  multiple lines comments
+
+  Datatypes:
+  String
+  Numbers
+  Boolean
+   Bigint
+
+  Null
+    Undefined
+   
+    Symbol
+
+    //Object
+*/
+
+//var score=102;
+
+let score=102;
+let name="Sanjana";
+let isLoggedin = false;
+
+//object
+let teaTypes =["lemon tea","ginger tea","green tea"];
+let user ={firstname:"sanjana",lastname:"sabat"};
+
+let getScore = score
+
+console.log(score);
+console.log(name);
+console.log(isLoggedin);
+console.log(teaTypes);
+console.log(user);
+console.log(getScore);
